92858ec6a3afca4440489f80d5df1c6336418e56
Tobias <twadseth@icloud.com>
2020-10-30 02:20:24 +0100

    Removed old conditional comments :bug:

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const app = express();
 app.use(cookieParser());
 app.use(express.static('./public'));
 
-// app.use((req, res) => {
-//     console.log(req.path);
-//     res.status(404).send('404: Page not found')
-// });
-
 const views = ['index', 'contact', 'resume', 'works'];
 
 app.get('/:lang?/:view?/:project?', (req, res) => {
@@ -35,31 +30,6 @@ app.get('/:lang?/:view?/:project?', (req, res) => {
         res.sendFile(__dirname + `/static/${view}-en.html`);
     }
 });
-
-// app.get('/verify', (req, res, next) => {
-//     console.info('Auth and redirect');
-//     res.redirect('/user');
-// })
-
-// app.get('/user', (req, res) => {
-//     res.send('User authenticated');
-// })
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/static/index-en.html');
-// });
-
-// app.get('/resume', (req, res) => {
-//     res.sendFile(__dirname + '/static/resume-en.html');
-// });
-
-// app.get('/contact', (req, res) => {
-//     res.sendFile(__dirname + '/static/contact-en.html');
-// });
-
-// app.get('/works', (req, res) => {
-//     res.sendFile(__dirname + '/static/works-en.html');
-// });
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
